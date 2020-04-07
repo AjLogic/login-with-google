17463d21913b8d5fe456671a18177ccc9181c681
ranade.ajinkya1@gmail.com <AjLogic>
2020-04-08 00:47:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { FormGroup,  FormBuilder, Validators } from '@angular/forms';
+import { AuthServiceConfig, GoogleLoginProvider, AuthService, SocialUser } from 'angularx-social-login';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'login-with-google';
+export class AppComponent implements OnInit{
+    loginForm: FormGroup;
+    user: SocialUser;
+    constructor(
+      private formBuilder: FormBuilder,
+      private authService: AuthService
+    ){}
+
+    ngOnInit() {
+    this.intiForm();
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(user);
+    });
+    }
+  
+  intiForm() {
+    this.loginForm = this.formBuilder.group({
+      firstName: [],
+      lastName: []
+    });
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(x => this.loginForm.patchValue(x)
+    );
+    
+  }
+  
 }
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("")
+  }
+]);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent} from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatIconModule, MatFormFieldModule, MatInputModule, MatButtonToggleModule, MatButtonModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider, SocialLoginModule  } from 'angularx-social-login';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +15,39 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SocialLoginModule,
+    MatButtonToggleModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('301346433116-8dh4oo2vqmkau179uvcqclt9jh8rd2m7.apps.googleusercontent.com')
+  },
+  
+]);
+
+export function provideConfig() {
+  return config;
+}
